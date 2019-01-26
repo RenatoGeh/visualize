80d75d7aa4e1127fcee88892b84cf0212e04bf85
Renato Lui Geh <renatogeh@gmail.com>
2019-01-26 02:23:58 -0200

    Adding sum node colorization
    Signed-off-by: Renato Lui Geh <renatogeh@gmail.com>

--- scope.go
+++ scope.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"github.com/RenatoGeh/gospn/spn"
 	"github.com/lucasb-eyer/go-colorful"
@@ -10,6 +11,8 @@ import (
 )
 
 const ScopeThreshold = 200
+
+var LearnType string
 
 func colorify(path string, Sc [][]int, C []colorful.Color, V spn.VarSet, idx int) {
 	I := image.NewRGBA(image.Rect(0, 0, Width, Height))
@@ -35,6 +38,97 @@ func colorify(path string, Sc [][]int, C []colorful.Color, V spn.VarSet, idx int
 	if err = png.Encode(f, I); err != nil {
 		fmt.Println(err)
 	}
+}
+
+type heapElement struct {
+	p float64
+	M spn.VarSet
+	i int
+}
+type maxHeap []*heapElement
+
+func (h maxHeap) Len() int           { return len(h) }
+func (h maxHeap) Less(i, j int) bool { return h[i].p > h[j].p }
+func (h maxHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].i = i
+	h[j].i = j
+}
+func (h *maxHeap) Push(e interface{}) {
+	i := e.(*heapElement)
+	i.i = len(*h)
+	*h = append(*h, i)
+}
+func (h *maxHeap) Pop() interface{} {
+	o := *h
+	n := len(o)
+	e := o[n-1]
+	e.i = -1
+	*h = o[0 : n-1]
+	return e
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func completify(path string, S spn.SPN, idx int) {
+	fmt.Printf("%d: Completify started\n", idx)
+	ch := S.Ch()
+	var H maxHeap
+	for i, c := range ch {
+		p, M := spn.MAP(c, make(spn.VarSet))
+		heap.Push(&H, &heapElement{
+			p: p,
+			M: M,
+			i: i,
+		})
+	}
+	var n int
+	if LearnType == "gens" {
+		n = min(H.Len(), 3)
+	} else {
+		n = 1
+	}
+	for i := 0; i < n && H.Len() > 0; i++ {
+		fmt.Printf("%d: Completing child %d\n", idx, i)
+		e := heap.Pop(&H).(*heapElement)
+		I := image.NewRGBA(image.Rect(0, 0, Width, Height))
+		for p, v := range e.M {
+			x, y := p%Width, p/Width
+			// Order: Blue, Green, Red
+			u := float64(v) / float64(Max)
+			if LearnType == "gens" {
+				I.Set(x, y, colorful.Color{
+					float64((((i + 1) >> 1) ^ 1)) * u,
+					float64((i & 1)) * u,
+					float64(((i & 2) >> 1)) * u,
+				})
+			} else {
+				I.Set(x, y, colorful.Color{u, u, u})
+			}
+		}
+		var filename string
+		if LearnType == "gens" {
+			filename = fmt.Sprintf("%s%d_%d.png", path, idx, i)
+		} else {
+			filename = fmt.Sprintf("%s%d.png", path, idx)
+		}
+		f, err := os.Create(filename)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%d: Drawing child %d...\n", idx, i)
+		if err = png.Encode(f, I); err != nil {
+			fmt.Println(err)
+		}
+		f.Close()
+	}
+	fmt.Printf("%d: Completify ended\n", idx)
 }
 
 func ColorScope(S spn.SPN, mdl string, is int, T []map[int]int) {
@@ -72,10 +166,20 @@ func ColorScope(S spn.SPN, mdl string, is int, T []map[int]int) {
 			return -1
 		}
 		if t := Z.Type(); t == "sum" {
-			//colorify(spath, Sc, C, T[0], sums)
-			sums++
+			if LearnType == "gens" {
+				completify(spath, Z, sums)
+				sums++
+			}
 		} else if t == "product" {
 			colorify(ppath, Sc, C, MeanImage(T), prods)
+			if LearnType == "dennis" {
+				for i, c := range ch {
+					if c.Type() == "sum" && len(Sc[i]) > ScopeThreshold {
+						completify(fmt.Sprintf("%s%d_", spath, prods), c, sums)
+						sums++
+					}
+				}
+			}
 			prods++
 		}
 		return 0

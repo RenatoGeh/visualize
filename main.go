c0b2cda363c58b1a8e6f22c9cfa101254f580699
Renato Lui Geh <renatogeh@gmail.com>
2019-01-26 02:24:13 -0200

    Changing CLI parameters
    Signed-off-by: Renato Lui Geh <renatogeh@gmail.com>

--- main.go
+++ main.go
@@ -11,34 +11,38 @@ import (
 
 func main() {
 	nargs := len(os.Args)
-	if nargs < 2 || nargs > 4 {
-		fmt.Printf("Usage: %s [model filename] | [filename]\n", os.Args[0])
+	if nargs < 4 || nargs > 5 {
+		fmt.Printf("Usage: %s [--train | --load] [model filename]\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	var S []spn.SPN
-	R, L, T, J, Sc, _ := ImagesToData("data/caltech_simple", -1, 39, 100, 100, 255)
+	R, L, T, J, Sc, _ := ImagesToData("data/caltech_simple", -1, 5, 100, 100, 7)
 	//R, L, T, J, Sc, _ := ImagesToData("data/english_small", -1, 5, 100, 100, 255)
 	fmt.Printf("|R|: %d, |L|: %d, |T|: %d, |J|: %d, |Sc|: %d\n", len(R), len(L), len(T), len(J), len(Sc))
-	if nargs == 2 {
+	LearnType = os.Args[2]
+	filename := os.Args[3]
+	fmt.Printf("Learning type: %s\n", LearnType)
+	fmt.Printf("Filename: %s\n", filename)
+	if os.Args[1] == "--load" {
 		fmt.Println("Loading SPNs...")
-		S = Load(os.Args[1])
-	} else if nargs == 3 {
+		S = Load(filename)
+	} else if os.Args[1] == "--train" {
 		fmt.Println("Extracting images...")
 		fmt.Println("Learning...")
-		if os.Args[1] == "gens" {
+		if LearnType == "gens" {
 			S = Gens(R, L, Sc)
-		} else if os.Args[1] == "dennis" {
+		} else if LearnType == "dennis" {
 			S = Dennis(R, L, Sc)
 		} else {
-			log.Fatalf("No architecture named %s.", os.Args[1])
+			log.Fatalf("No architecture named %s.", LearnType)
 		}
 		fmt.Println("Saving model...")
-		Save(S, os.Args[2])
+		Save(S, filename)
 	}
-	if nargs == 4 && os.Args[3] == "cmpl" {
+	if nargs == 5 && os.Args[4] == "cmpl" {
 		if S == nil {
-			S = Load(os.Args[1])
+			S = Load(filename)
 		}
 		fmt.Println("Performing completion...")
 		CompleteData(S, T, J)
@@ -48,7 +52,7 @@ func main() {
 		Q := conc.NewSingleQueue(-1)
 		for i := range S {
 			Q.Run(func(id int) {
-				ColorScope(S[id], os.Args[1], id, R[id])
+				ColorScope(S[id], filename, id, R[id])
 			}, i)
 		}
 		Q.Wait()
